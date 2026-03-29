fa2bca9dba1931e3faf7a9b716e51870d59e87cf
JuanDQDev <juand.quitianr@uqvirtual.edu.co>
2026-03-28 22:32:29 -0500

    Creacion menu de registro, navegacion entre los 3 primeros menus completada

--- src/main/java/utilities/Paths.java
+++ src/main/java/utilities/Paths.java
@@ -4,6 +4,7 @@ public class Paths {
 
     public static final String LOGIN_PAGE = "/loginPage.fxml";
     public static final String MAIN_PAGE = "/principalMenu.fxml";
+    public static final String REGISTER_PAGE = "/registerMenu.fxml";
 
 
 }
