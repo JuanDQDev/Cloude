678880aeadf12ca8e8b8e7a934653e0dea48b806
alejandrotovarf-stack <alejandro.tovarf@uqvirtual.edu.co>
2026-04-30 23:23:08 -0500

    mapa y pantalla para escoger el plan

--- src/main/java/utilities/Paths.java
+++ src/main/java/utilities/Paths.java
@@ -6,6 +6,6 @@ public class Paths {
     public static final String MAIN_PAGE = "/principalMenu.fxml";
     public static final String REGISTER_PAGE = "/registerMenu.fxml";
     public static final String MAIN_MENU = "/mainMenu.fxml";
-
+    public static final String MAPA_PARQUE_2= "/mapaParque.fxml";
 
 }
